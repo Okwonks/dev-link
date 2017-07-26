76e1940c9ab63ea83fe10e3d0f55728425993835
Okwonks <albertoketch@gmail.com>
2017-07-26 15:04:52 +0300

    Commenting out the footer to change the look of the project.

--- app/routes/developer-detail.js
+++ app/routes/developer-detail.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
-import config from '../config/environment';
+// import config from '../config/environment';
 
 export default Ember.Route.extend({
     modal(params) {
     var key = "e7f7c50800fd4bd7620f8eaa9719708bcc8b1346"
     var url = 'https://api.github.com/users/d-kahara/repos?access_token=' + key;
     return Ember.RSVP.hash({
-        developers: this.store.findRecord('developer', params.developer_id),
+        developer: this.store.findRecord('developer', params.developer_id),
         repos: Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON;
       })
