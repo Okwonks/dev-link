e00728f6b67b3ac01f37b380cae22d70effa0976
waracci <morriswarachi0@gmail.com>
2017-07-26 15:21:10 +0300

    all

---
+++ app/models/language.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    name: DS.attr(),
+    shortName: DS.attr(),
+    rank: DS.attr(),
+    description: DS.attr(),
+    year: DS.attr(),
+    linkToResources: DS.attr(),
+    image: DS.attr(),
+    likes: DS.attr(),
+    dislikes: DS.attr()
+});
--- app/routes/developer-detail.js
+++ app/routes/developer-detail.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     modal(params) {
         return this.store.findRecord('developer', params.developer_id);
-    }
+    },
+      model() {
+    var key = "e7f7c50800fd4bd7620f8eaa9719708bcc8b1346"
+    var userName = "d-kahara"
+    var url = 'https://api.github.com/users/'+ userName +'/repos?access_token=' + key;
+        return Ember.$.getJSON(url).then(function(responseJSON) {
+        return responseJSON;
+      })
+  }
 });
