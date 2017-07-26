51882fbba187e42e6c4b5da6607d5a0be4f66671
David Kahara <kaharadave@gmail.com>
2017-07-26 11:49:23 +0300

    Modified the user Interface on Developer Detail

--- app/routes/developer-detail.js
+++ app/routes/developer-detail.js
@@ -1,25 +1,13 @@
 import config from '../config/environment';
 import Ember from 'ember';
 
-
-
 export default Ember.Route.extend({
+
   model() {
-    var key = 'e7f7c50800fd4bd7620f8eaa9719708bcc8b1346';
-
-
-    var url2 = 'https://api.github.com/users/d-kahara/repos?access_token=' + key;
+    var key = "e7f7c50800fd4bd7620f8eaa9719708bcc8b1346"
+    var url = 'https://api.github.com/users/d-kahara/repos?access_token=' + key;
     return Ember.RSVP.hash({
-       repos: Ember.$.getJSON(url2).then(function(responseJSON) {
-         return responseJSON;
-       })
-     }
-   )},
-
-  model2() {
-    var url = 'https://api.github.com/users/d-kahara?access_token=e7f7c50800fd4bd7620f8eaa9719708bcc8b1346';
-    return Ember.RSVP.hash({
-        users: Ember.$.getJSON(url2).then(function(responseJSON) {
+        repos: Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON;
 
       })
