f56f8b02cdaa5774fc76ce7f8794dbda59047b0f
Okwonks <albertoketch@gmail.com>
2017-07-26 07:40:41 +0300

    Editing files after merge error. Some dependencies needed to be reinstalled.

--- app/routes/developer.js
+++ app/routes/developer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return this.store.findAll('developer');
+    },
+    actions: {
+        saveDeveloper(params) {
+            var newDeveloper = this.store.createRecord('developer', params);
+            newDeveloper.save();
+            this.transitionTo('developer');
+        }
+    }
 });
