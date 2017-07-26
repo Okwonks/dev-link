8bd8191a8fa010f843ee15caa59035944577ec73
waracci <morriswarachi0@gmail.com>
2017-07-26 12:06:39 +0300

    added favicons

--- app/models/developer.js
+++ app/models/developer.js
@@ -5,8 +5,4 @@ export default DS.Model.extend({
     lastName: DS.attr(),
     email: DS.attr(),
     github: DS.attr()
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> d2b9ac79051ffdb4bca3be3da0f7ce72c7ab3e28
