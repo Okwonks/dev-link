16f63d9bec2e8f5f6b0d2d8d6120c4c1b56aa000
Okwonks <albertoketch@gmail.com>
2017-07-27 14:49:01 +0300

    Editing the index page to have more content that will allow people to land on the page and have an idea what Dev-link is all about.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return Ember.RSVP.hash({
+      developers: this.store.query('developer', {
+        orderBy: 'github',
+        limitToFirst: 6
+      }),
+      questions: this.store.query('question', {
+          orderBy: 'author',
+          limitToLast: 4
+      })
+    })
+  }
 });
