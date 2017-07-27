6f9d64a51ddc2d0547a1f9432e5f4e3359c6220f
Okwonks <albertoketch@gmail.com>
2017-07-27 14:38:33 +0300

    Adding a computed property for the users to be able to have their first and last name joined.

--- app/components/developer-tile.js
+++ app/components/developer-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    name: Ember.computed('developer.firstName', 'developer.lastName', function() {
+        return this.get('developer.firstName') + ' ' + this.get('developer.lastName');
+    })
 });
