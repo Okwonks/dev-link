58f6bd51d96f0ef6ae556abcf6604a9ae21d5111
David Kahara <kaharadave@gmail.com>
2017-07-27 20:32:30 +0300

    fixed bug with login

--- app/routes/developer-detail.js
+++ app/routes/developer-detail.js
@@ -4,22 +4,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
       return this.store.findRecord('developer',params.developer_id)
-    }
-// actions: {
-//
-//   developer.get('github')
-// }  //   var key = "e7f7c50800fd4bd7620f8eaa9719708bcc8b1346"
-  //   //var userName = this.store.findRecord('developer', params.github)
-  //   var userName = developer.get('github')
-  //
-  //   console.log(userName);
-  //   var url = 'https://api.github.com/users/'+ userName +'/repos?access_token=' + key;
-  //  return Ember.RSVP.hash({
-  //    repos: Ember.$.getJSON(url).then(function(responseJSON) {
-  //    return responseJSON;
-  //  })
-  //  })
-  //       return
-  // }
+    },
 
+      showClicked: false,
+ actions: {
+   getRepos(model) {
+     this.set('showClicked', true);
+     var key = "e7f7c50800fd4bd7620f8eaa9719708bcc8b1346"
+      var userName = model.get('github')
+      console.log(userName);
+     var url = 'https://api.github.com/users/' + userName + '/repos?access_token=' + key;
+     console.log(url);
+     return Ember.$.getJSON(url).then(function(reposs){
+        reposs.forEach(function(repos) {
+          console.log(repos.name);
+          return repos
+  });
+     })
+   }
+  }
 });
