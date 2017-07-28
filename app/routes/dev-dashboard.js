5f12df2b41c93d65545e355d3ace39c592af2ce6
Christine Wasike <christinewasike22@gmail.com>
2017-07-28 04:06:07 +0300

    pulled from kaara

--- app/routes/dev-dashboard.js
+++ app/routes/dev-dashboard.js
@@ -4,21 +4,5 @@ export default Ember.Route.extend({
   model: function (params) {
     return this.store.find('developer', params.developer_id);
   },
-  editDevDetails: false,
-  actions: {
-    editDevDetails() {
-      this.set('editDevDetails', true);
-    },
-    saveChanges() {
-      var params = {
-        firstName: this.get('firstName'),
-        lastName: this.get('lastName'),
-        email: this.get('email'),
-        github: this.get('github'),
-        password: this.get('password')
-      };
-      this.set('editDevDetails', false);
-      this.sendAction('update', rental, params);
-    }
-  }
+
 });
